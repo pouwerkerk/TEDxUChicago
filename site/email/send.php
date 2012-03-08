7b0856c5b998ad85a0e33bcc2af912bbaebc0344
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2012-03-07 22:31:25 -0600

    Merging changes from live site and local versions

--- site/email/send.php
+++ site/email/send.php
@@ -20,17 +20,17 @@ function sendMessage($email_address, $email_name, $email_subject, $email_content
 		$mail->Host = "ssl://smtp.gmail.com"; // sets the SMTP server
 		$mail->Port = 465;										// set the SMTP port for the GMAIL server
 		
-		$mail->Username = "ubazaar.uchicago@gmail.com"; // SMTP account username
-		$mail->Password = "yB1Id7pmPnLic1cMXOCQ";				// SMTP account password
-		$mail->AddReplyTo('ubazaar.uchicago@gmail.com', 'uBazaar');
+		$mail->Username = "info@tedxuchicago.com"; // SMTP account username
+		$mail->Password = "tedxuchicago2012";				// SMTP account password
+		$mail->AddReplyTo('info@tedxuchicago.com', 'TEDxUChicago');
 		$mail->AddAddress($email_address, $email_name);
-		$mail->SetFrom('ubazaar.uchicago@gmail.com', 'uBazaar');
-		$mail->AddReplyTo('ubazaar.uchicago@gmail.com', 'uBazaar');
+		$mail->SetFrom('info@tedxuchicago.com', 'TEDxUChicago');
+		$mail->AddReplyTo('info@tedxuchicago.com', 'TEDxUChicago');
 		if($bccE) {
 			$mail->AddBCC($bccE, $bccN);
 		} 
 		$mail->Subject = $email_subject;
-		$mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
+		// $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
 		$mail->MsgHTML($HTML_content);
 	//	$mail->AddAttachment('images/phpmailer.gif');		// attachment
 	//	$mail->AddAttachment('images/phpmailer_mini.gif'); // attachment
