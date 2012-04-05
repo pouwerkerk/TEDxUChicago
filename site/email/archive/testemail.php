d06b17d889d2aee08c7d055e8908d0f0a82ebbde
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2012-04-04 22:07:02 -0600

    Changing the structure of the site

--- site/email/archive/testemail.php
+++
@@ -1,4 +0,0 @@
-<?
-mail("t3tech@gmail.com","Testing","From: test@test.com");
-echo " message sent succesfully";
-?>
--- site/register/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-header("Location: http://tedxuchicago.com/application");
-
-?>
