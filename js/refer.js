efa4ba2271e3eb095e2f18737b3a3c9c0f87c319
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2012-04-15 10:51:59 -0500

    Built controller for form (refer.js)

--- js/refer.js
+++ js/refer.js
@@ -3,7 +3,30 @@ function center(id, constant) {
   $(id).css({marginTop: (posTop + "px")});
 }
 
+function showResult(data) {
+  if (data.result == "success") {
+	$(".name").fadeOut(250);
+	$(".email").fadeOut(250);
+	$(".url").fadeIn(250);
+	$("#url").val("http://ubazaar.uchicago.edu/seller/tedxuchicago/?ref=" + data.code);
+  } else {
+  	$("form").replaceWith("Error: " + data.error);
+  }
+}
+
 $(document).ready(function() {
   center("#container", 150);
+  $("#submit").click(function(e) {
+    e.preventDefault();
+    $(this).attr("disabled");
+    $.ajax({
+      url: "action.php",
+      dataType: 'json',
+      data: $("#codeRetrieve").serialize(),
+      success: function(data) {
+        showResult(data);
+      }
+    });
+  });
 });
 
